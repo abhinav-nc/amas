4247fd235b63ac70e9d7ac7eadd26d39c489d569
abhinav-nc <abhinav@neuralcraft.co>
2026-02-27 15:30:18 +0530

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 const config = {
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
       fallback: 'index.html'
-    })
+    }),
+
+    paths: {
+      base: dev ? '' : '/amas'
+    }
   }
 };
 
